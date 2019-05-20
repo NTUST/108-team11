22eb54d6e2b9fba47adc52462027693b3f88c255
timmmmmmm <33017897+timmmmmmm@users.noreply.github.com>
2019-05-20 10:49:39 +0800

    0520

--- game/mysite/views.py
+++ game/mysite/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render
 from django.template.loader import get_template
 from django.http import HttpResponse
 
 # Create your views here.
 
-def homePage():
+def homePage(request):
 	template = get_template('startGame.html')
 	html = template.render(locals())
 	return HttpResponse(html)
