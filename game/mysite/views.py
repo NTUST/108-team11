8310019262446b0421fc144487ec3329e3366930
timmmmmmm <33017897+timmmmmmm@users.noreply.github.com>
2019-05-25 10:17:22 +0800

    0525 karen and emico line almost finish

--- game/mysite/views.py
+++ game/mysite/views.py
@@ -352,5 +352,328 @@ def gameOver(request):
 
 
 
+#karen 1-2 grade
+
+def startk(request):
+	return render(request, '1-2/karen/1start.html')
+
+def eatopk(request):
+	return render(request, '1-2/karen/2eatop.html')
+
+def eatgoodk(request):
+	return render(request, '1-2/karen/3-1eatgood.html')
+
+def eatgood2k(request):
+	return render(request, '1-2/karen/3-2eatgood2.html')
+
+def eatgood3k(request):
+	return render(request, '1-2/karen/3-3eatgood3.html')
+
+def eatnok(request):
+	return render(request, '1-2/karen/3-4eatno.html')
+
+def class1k(request):
+	return render(request, '1-2/karen/4class.html')
+
+def classopk(request):
+	return render(request, '1-2/karen/5classop.html')
+
+def classbookk(request):
+	return render(request, '1-2/karen/6-1classbook.html')
+
+def classvideok(request):
+	return render(request, '1-2/karen/6-2classvideo.html')
+
+def classsleepk(request):
+	return render(request, '1-2/karen/6-3classsleep.html')
+
+def homek(request):
+	return render(request, '1-2/karen/7home.html')
+
+def homeopk(request):
+	return render(request, '1-2/karen/8homeop.html')
+
+def homeop101k(request):
+	return render(request, '1-2/karen/9-1homeop1-1.html')
+
+def eatgood4k(request):
+	return render(request, '1-2/karen/9-2-1eatgood4.html')
+
+def eatgood5k(request):
+	return render(request, '1-2/karen/9-2-2eatgood5.html')
+
+def eatgood6k(request):
+	return render(request, '1-2/karen/9-2-3eatgood6.html')
+
+def eatgood7k(request):
+	return render(request, '1-2/karen/9-2-4eatgood7.html')
+
+def eatop2k(request):
+	return render(request, '1-2/karen/9-2eatop2.html')
+
+def homeop103k(request):
+	return render(request, '1-2/karen/9-3homeop1-3.html')
+
+def homeop104k(request):
+	return render(request, '1-2/karen/9-4homeop1-4.html')
+
+def clubk(request):
+	return render(request, '1-2/karen/10club.html')
+
+def club1k(request):
+	return render(request, '1-2/karen/11-1club1.html')
+
+def club2k(request):
+	return render(request, '1-2/karen/11-2club2.html')
+
+def club3k(request):
+	return render(request, '1-2/karen/11-3club3.html')
+
+def loverk(request):
+	return render(request, '1-2/karen/12lover.html')
+
+def loveropk(request):
+	return render(request, '1-2/karen/13loverop.html')
+
+def loverlovek(request):
+	return render(request, '1-2/karen/14-1-1loverlove.html')
+
+def loverlove2k(request):
+	return render(request, '1-2/karen/14-1-2loverlove2.html')
+
+def loverlove3k(request):
+	return render(request, '1-2/karen/14-1-3loverlove3.html')
+
+def lovertalkk(request):
+	return render(request, '1-2/karen/14-2lovertalk.html')
+
+def campk(request):
+	return render(request, '1-2/karen/15camp.html')
+
+def campgok(request):
+	return render(request, '1-2/karen/16campgo.html')
+
+def midtermk(request):
+	return render(request, '1-2/karen/17midterm.html')
+
+def midtermopk(request):
+	return render(request, '1-2/karen/18midtermop.html')
+
+def promk(request):
+	return render(request, '1-2/karen/19prom.html')
+
+def promgok(request):
+	return render(request, '1-2/karen/20promgo.html')
+
+def finalk(request):
+	return render(request, '1-2/karen/21final.html')
+
+def winterVacation1k(request):
+	return render(request, '1-2/karen/22winterVacation1.html')
+
+def winterVacation101k(request):
+	return render(request, '1-2/karen/23-1winterVacation1-1.html')
+
+def winterVacation102k(request):
+	return render(request, '1-2/karen/23-2winterVacation1-2.html')
+
+def winterVacationEnd1k(request):
+	return render(request, '1-2/karen/24winterVacationEnd1.html')
+
+def ktvk(request):
+	return render(request, '1-2/karen/25ktv.html')
+
+def ktvgok(request):
+	return render(request, '1-2/karen/26ktvgo.html')
+
+def secondDrop1k(request):
+	return render(request, '1-2/karen/27secondDrop1.html')
+
+def secondDrop1okk(request):
+	return render(request, '1-2/karen/28secondDrop1ok.html')
+
+def summerVacation1k(request):
+	return render(request, '1-2/karen/29summerVacation1.html')
+
+def summerVacation101k(request):
+	return render(request, '1-2/karen/30-1summerVacation1-1.html')
+
+def summerVacation102k(request):
+	return render(request, '1-2/karen/30-2summerVacation1-2.html')
+
+def summerVacation103k(request):
+	return render(request, '1-2/karen/30-3summerVacation1-3.html')
+
+def summerVacation104k(request):
+	return render(request, '1-2/karen/30-4summerVacation1-4.html')
+
+def rentHousek(request):
+	return render(request, '1-2/karen/31rentHouse.html')
+
+def sophomoreOpk(request):
+	return render(request, '1-2/karen/32sophomoreOp.html')
+
+def sophomoreOp101k(request):
+	return render(request, '1-2/karen/33-1sophomoreOp1-1.html')
+
+def sophomoreOp102k(request):
+	return render(request, '1-2/karen/33-2sophomoreOp1-2.html')
+
+def sophomoreOp103k(request):
+	return render(request, '1-2/karen/33-3sophomoreOp1-3.html')
+
+def sophomoreOp104k(request):
+	return render(request, '1-2/karen/33-4sophomoreOp1-4.html')
+
+def lover2k(request):
+	return render(request, '1-2/karen/34lover2.html')
+
+def loverop2k(request):
+	return render(request, '1-2/karen/35loverop2.html')
+
+def loverokk(request):
+	return render(request, '1-2/karen/36-1loverok.html')
+
+def loverEndk(request):
+	return render(request, '1-2/karen/36-2loverEnd.html')
+
+def secondDrop2k(request):
+	return render(request, '1-2/karen/37secondDrop2.html')
+
+def secondDrop2okk(request):
+	return render(request, '1-2/karen/38secondDrop2ok.html')
+
+def winterVacation2k(request):
+	return render(request, '1-2/karen/39winterVacation2.html')
+
+def winterVacation201k(request):
+	return render(request, '1-2/karen/40-1winterVacation2-1.html')
+
+def winterVacation202k(request):
+	return render(request, '1-2/karen/40-2winterVacation2-2.html')
+
+def sophomore2k(request):
+	return render(request, '1-2/karen/41sophomore2.html')
+
+def finalExam2k(request):
+	return render(request, '1-2/karen/42finalExam2.html')
+
+def summerVacation2k(request):
+	return render(request, '1-2/karen/43summerVacation2.html')
+
+def summerVacation201k(request):
+	return render(request, '1-2/karen/44-1summerVacation2-1.html')
+
+def summerVacation202k(request):
+	return render(request, '1-2/karen/44-2summerVacation2-2.html')
+
+def summerVacation203k(request):
+	return render(request, '1-2/karen/44-3summerVacation2-3.html')
+
+def summerVacation204k(request):
+	return render(request, '1-2/karen/44-4summerVacation2-4.html')
+
+#emico 3-4
+
+def rdGradek(request):
+	return render(request, '3-4/karen/45_3rdGrade.html')
+
+def rdGradeOpk(request):
+	return render(request, '3-4/karen/46_3rdGradeOp.html')
+
+def exchangeOpk(request):
+	return render(request, '3-4/karen/47_exchangeOp.html')
+
+def countriesOpk(request):
+	return render(request, '3-4/karen/47-1_countriesOp.html')
+
+def USAk(request):
+	return render(request, '3-4/karen/47-1-1_USA.html')
+
+def Australiak(request):
+	return render(request, '3-4/karen/47-1-2_Australia.html')
+
+def Germanyk(request):
+	return render(request, '3-4/karen/47-1-3_Germany.html')
+
+def Francek(request):
+	return render(request, '3-4/karen/47-1-4_France.html')
+
+def exchangeNk(request):
+	return render(request, '3-4/karen/47-2_exchangeN.html')
+
+def exchangeYk(request):
+	return render(request, '3-4/karen/48_exchangeY.html')
+
+def volleyballCaptonOpk(request):
+	return render(request, '3-4/karen/49_volleyballCaptonOp.html')
+
+def captonYk(request):
+	return render(request, '3-4/karen/49-1_captonY.html')
+
+def captonNk(request):
+	return render(request, '3-4/karen/49-2_captonN.html')
+
+def summerVacation3Opk(request):
+	return render(request, '3-4/karen/50_summerVacation3Op.html')
+
+def summerVacation31k(request):
+	return render(request, '3-4/karen/50-1_summerVacation3.html')
+
+def summerVacation32k(request):
+	return render(request, '3-4/karen/50-2_summerVacation3.html')
+
+def summerVacation33k(request):
+	return render(request, '3-4/karen/50-3_summerVacation3.html')
+
+def summerVacation34k(request):
+	return render(request, '3-4/karen/50-4_summerVacation3.html')
+
+def seniork(request):
+	return render(request, '3-4/karen/51_senior.html')
+
+def seniorOpk(request):
+	return render(request, '3-4/karen/52_seniorOp.html')
+
+def internshipOpk(request):
+	return render(request, '3-4/karen/52-1_internshipOp.html')
+
+def masterOpk(request):
+	return render(request, '3-4/karen/52-2_masterOp.html')
+
+def takeExamk(request):
+	return render(request, '3-4/karen/53_takeExam.html')
+
+def workk(request):
+	return render(request, '3-4/karen/53_work.html')
+
+def finalExamOpk(request):
+	return render(request, '3-4/karen/54_finalExamOp.html')
+
+def graduationk(request):
+	return render(request, '3-4/karen/55_graduation.html')
+
+def finishGamek(request):
+	return render(request, '3-4/karen/56_finishGame.html')
+
+
+
+
+
+
+
+#
+
+def startt(request):
+	return render(request, '1-2/timmy/1start.html')
+
+
+
+
+
+
+
+
+
 
 
