e78d39d7506f592882842eb943d1a88ea89be185
leo8883 <48037911+leo8883@users.noreply.github.com>
2019-07-10 18:50:37 +0800

    Summary

--- game/mysite/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from mysite.models import user
-# Register your models here.
-
-class UserAdmin(admin.ModelAdmin):
-	list_display = ['name','date', 'family', 'education', 'friendship', 'health', 'wealth', 'love']
-		
-
-admin.site.register(user, UserAdmin)
